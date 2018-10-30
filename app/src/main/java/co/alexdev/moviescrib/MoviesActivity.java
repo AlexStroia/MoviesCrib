7edded109dceaec376f1f00350b8dde75d1a5a49
Alex Stroia <salexandru995@gmail.com>
2018-10-29 23:07:14 -0700

    Added RecyclerView & Custom toolbar - DONE

--- app/src/main/java/co/alexdev/moviescrib/MoviesActivity.java
+++ app/src/main/java/co/alexdev/moviescrib/MoviesActivity.java
@@ -2,12 +2,23 @@ package co.alexdev.moviescrib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MoviesActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setup();
+    }
+
+    //TODO Setup all the content in this function
+    private void setup() {
     }
 }
