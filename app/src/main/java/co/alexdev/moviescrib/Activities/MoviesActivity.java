8301278bc3900f7047e31120ea994f60e6998166
Alex Stroia <salexandru995@gmail.com>
2018-10-30 12:51:19 -0700

    Added support for retrofit, picasso & Adapter setup - DONE

--- app/src/main/java/co/alexdev/moviescrib/Activities/MoviesActivity.java
+++ app/src/main/java/co/alexdev/moviescrib/Activities/MoviesActivity.java
@@ -2,20 +2,60 @@ package co.alexdev.moviescrib.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import co.alexdev.moviescrib.Adapter.MoviesAdapter;
+import co.alexdev.moviescrib.Model.Movie;
 import co.alexdev.moviescrib.R;
 
-public class MoviesActivity extends AppCompatActivity {
+public class MoviesActivity extends AppCompatActivity implements MoviesAdapter.onMovieClickListener {
+
+    private RecyclerView rv_movies;
+    private GridLayoutManager gridLayoutManager;
+    private ArrayList<Movie> mMoviesArray = new ArrayList<>();
+
+    private Toast mToast;
+    private MoviesAdapter mMoviesAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
 
-        setup();
+        rv_movies = findViewById(R.id.rv_movies);
+
+        setupRecyclerView();
     }
 
-    //TODO Setup all the content in this function
-    private void setup() {
+    private void setupRecyclerView() {
+        Movie movie = new Movie("Avengers", 5.6, "11.05.1995", "www.google.ro", "Awesome movie");
+        mMoviesArray.add(movie);
+        mMoviesArray.add(movie);
+        mMoviesArray.add(movie);
+        mMoviesArray.add(movie);
+        mMoviesArray.add(movie);
+        mMoviesArray.add(movie);
+
+
+        mMoviesAdapter = new MoviesAdapter(mMoviesArray, this);
+        gridLayoutManager = new GridLayoutManager(this, 2);
+
+        rv_movies.setLayoutManager(gridLayoutManager);
+        rv_movies.setAdapter(mMoviesAdapter);
+    }
+
+
+    @Override
+    public void onMovieClick(int position) {
+        if (mToast != null) {
+            mToast.cancel();
+        }
+        mToast = Toast.makeText(this, "Position: " + position, Toast.LENGTH_LONG);
+        mToast.show();
     }
 }
--- app/src/main/java/co/alexdev/moviescrib/Adapter/MoviesAdapter.java
+++ app/src/main/java/co/alexdev/moviescrib/Adapter/MoviesAdapter.java
@@ -1,31 +1,74 @@
 package co.alexdev.moviescrib.Adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import co.alexdev.moviescrib.Model.Movie;
+import co.alexdev.moviescrib.R;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder> {
 
+    private List<Movie> movieList;
+    private static onMovieClickListener mMovieClickListener;
+
+    public interface onMovieClickListener {
+        void onMovieClick(int position);
+    }
+
+    public MoviesAdapter(List<Movie> movieList, onMovieClickListener movieClickListener) {
+        this.movieList = movieList;
+        this.mMovieClickListener = movieClickListener;
+    }
+
     @Override
     public MoviesAdapter.MoviesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        final Context context = viewGroup.getContext();
+        final View rootView = LayoutInflater.from(context).inflate(R.layout.movies_list_item, viewGroup, false);
+        return new MoviesViewHolder(rootView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MoviesAdapter.MoviesViewHolder moviesViewHolder, int i) {
 
+        final String title = movieList.get(i).getTitle();
+        final String image = movieList.get(i).getImage();
+
+        moviesViewHolder.tv_movie_title.setText(title);
+        //TODO - LOAD WITH PICASSO IMAGE
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return (movieList != null && movieList.size() > 0 ? movieList.size() : 0);
     }
 
-    static class MoviesViewHolder extends RecyclerView.ViewHolder {
+    static class MoviesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView iv_movie;
+        TextView tv_movie_title;
 
         public MoviesViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            iv_movie = itemView.findViewById(R.id.iv_poster);
+            tv_movie_title = itemView.findViewById(R.id.tv_movie_name);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            final int position = getAdapterPosition();
+            mMovieClickListener.onMovieClick(position);
         }
     }
 }
