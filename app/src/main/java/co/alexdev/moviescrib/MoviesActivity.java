99282286af03262fea9a89ff2e095863044e3756
Alex Stroia <salexandru995@gmail.com>
2018-10-30 11:33:30 -0700

    Added movies_list_item design - DONE

--- app/src/main/java/co/alexdev/moviescrib/MoviesActivity.java
+++ app/src/main/java/co/alexdev/moviescrib/MoviesActivity.java
@@ -6,14 +6,10 @@ import android.support.v7.widget.Toolbar;
 
 public class MoviesActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
-
-        toolbar = findViewById(R.id.toolbar);
 
         setup();
     }
