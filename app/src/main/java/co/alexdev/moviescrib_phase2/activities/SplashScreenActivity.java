f0b81c1456e1bf7626fdc1b45e5c6069d546d528
Alex Stroia <salexandru995@gmail.com>
2018-11-24 09:44:47 -0800

    Added SplashScreenActivity & design & functionality for it

---
+++ app/src/main/java/co/alexdev/moviescrib_phase2/activities/SplashScreenActivity.java
@@ -0,0 +1,75 @@
+package co.alexdev.moviescrib_phase2.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import co.alexdev.moviescrib_phase2.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView iv_popcorn_splash;
+    ImageView iv_splash_logo;
+    ImageView iv_glasses_big;
+    Button btnGetStarted;
+
+    Animation alpha;
+    Animation fromBottom;
+    Animation fromTop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        iv_splash_logo = findViewById(R.id.ic_splash_logo);
+        iv_popcorn_splash = findViewById(R.id.iv_popcorn_splash);
+        iv_glasses_big = findViewById(R.id.iv_glasses_big);
+
+        setAnimations();
+    }
+
+    private void setAnimations() {
+        setBottonAnimation();
+        setTopAnimation();
+        setAlphaAnimation();
+    }
+
+    private void setBottonAnimation() {
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+        iv_popcorn_splash.setAnimation(fromBottom);
+    }
+
+    private void setTopAnimation() {
+        fromTop = AnimationUtils.loadAnimation(this, R.anim.fromtop);
+        iv_glasses_big.setAnimation(fromTop);
+    }
+
+    private void setAlphaAnimation() {
+        alpha = AnimationUtils.loadAnimation(this, R.anim.fade);
+        iv_splash_logo.setAnimation(alpha);
+        alpha.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashScreenActivity.this, BaseActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
